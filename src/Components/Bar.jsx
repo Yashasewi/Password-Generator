3f4b8d9335c456ad1f97b8f846657fa9779baccf
Yashasewi singh <32950332+Yashasewi@users.noreply.github.com>
2022-11-10 12:01:00 +0530

    added active bar

--- src/Components/Bar.jsx
+++ src/Components/Bar.jsx
@@ -4,22 +4,31 @@ import image3 from "../assets/Images/copy.png";
 import { useState } from "react";
 
 function Bar() {
-    const [length, setLength] = useState(7);
+    const [length, setLength] = useState(10);
 
     return (
-        <div className="bar">
-            <img src={image1} className="lock" alt="lock"/>
-            {/* <img src={image2} className="dots" alt="dots" srcset="" /> */}
-            {/* <div className="test">..........</div> */}
-            <div className="dots-input">
-                {new Array(length).fill(0).map((dot, ind) => {
-                    return <div key={ind} className="dot"></div>;
-                })}
+        <div>
+            <div className="bar display">
+                <img src={image1} className="lock" alt="lock" />
+                {/* <img src={image2} className="dots" alt="dots" srcset="" /> */}
+                {/* <div className="test">..........</div> */}
+                <div className="dots-input">
+                    {new Array(length).fill(0).map((dot, ind) => {
+                        return <div key={ind} className="dot"></div>;
+                    })}
+                </div>
+                <div className="element">
+                    <img src={image2} className="reset" alt="reset" />
+                    <img src={image3} className="copy" alt="copy" />
+                </div>
             </div>
-
-            <div className="element">
-                <img src={image2} className="reset" alt="reset" />
-                <img src={image3} className="copy" alt="copy" />
+            <div className="bar-active bar">
+                <img src={image1} className="lock" alt="lock" />
+                <div className="password ">HJSFHgh348$#5</div>
+                <div className="element">
+                    <img src={image2} className="reset" alt="reset" />
+                    <img src={image3} className="copy" alt="copy" />
+                </div>
             </div>
         </div>
     );
