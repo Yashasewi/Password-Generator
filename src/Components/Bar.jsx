b8795dcfd604df1c5ab637e483c79cf2bb3e980e
Yashasewi singh <32950332+Yashasewi@users.noreply.github.com>
2022-11-13 11:58:25 +0530

    random password genrater

--- src/Components/Bar.jsx
+++ src/Components/Bar.jsx
@@ -3,12 +3,45 @@ import image2 from "../assets/Images/Reset.png";
 import image3 from "../assets/Images/copy.png";
 import { useState } from "react";
 
+const Allowed = {
+    Uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
+    Lowers: "qwertyuiopasdfghjklzxcvbnm",
+    Numbers: "1234567890",
+    Symbols: "!$^&*-=+_?",
+};
+
+// password generator
+function generatePassword(length) {
+    const crypto = window.crypto || window.msCrypto;
+
+    if (typeof crypto === "undefined") {
+        throw new Error(
+            "Crypto API is not supported. Please upgrade your web browser"
+        );
+    }
+
+    const charset =
+        Allowed.Uppers + Allowed.Numbers + Allowed.Lowers + Allowed.Symbols;
+
+    const indexes = crypto.getRandomValues(new Uint32Array(length));
+
+    let secret = "";
+
+    for (const index of indexes) {
+        secret += charset[index % charset.length];
+    }
+
+    return secret;
+}
+
+const password = generatePassword(16);
+
 function Bar() {
     const [length, setLength] = useState(10);
 
     return (
         <div>
-            <div className="bar display">
+            <div className="bar ">
                 <img src={image1} className="lock" alt="lock" />
                 {/* <img src={image2} className="dots" alt="dots" srcset="" /> */}
                 {/* <div className="test">..........</div> */}
@@ -22,9 +55,9 @@ function Bar() {
                     <img src={image3} className="copy" alt="copy" />
                 </div>
             </div>
-            <div className="bar-active bar">
+            <div className="bar-active bar display">
                 <img src={image1} className="lock" alt="lock" />
-                <div className="password ">HJSFHgh348$#5</div>
+                <div className="password ">{password}</div>
                 <div className="element">
                     <img src={image2} className="reset" alt="reset" />
                     <img src={image3} className="copy" alt="copy" />
