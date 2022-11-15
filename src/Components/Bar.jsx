73c93e58c165e02c82ff8360a81ec1c5730186d2
Naman Baranwal <namanbaranwal2002@gmail.com>
2022-11-15 10:38:19 +0530

    Added Working Slider

--- src/Components/Bar.jsx
+++ src/Components/Bar.jsx
@@ -1,77 +1,73 @@
 import image1 from "../assets/Images/lock.png";
 import image2 from "../assets/Images/Reset.png";
 import image3 from "../assets/Images/copy.png";
-import { useState } from "react";
 
 const Allowed = {
-    Uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
-    Lowers: "qwertyuiopasdfghjklzxcvbnm",
-    Numbers: "1234567890",
-    Symbols: "!$^&*-=+_?",
+	Uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
+	Lowers: "qwertyuiopasdfghjklzxcvbnm",
+	Numbers: "1234567890",
+	Symbols: "!$^&*-=+_?",
 };
-
 
 // password generator
 function generatePassword(
-    length,
-    charset = Allowed.Uppers +
-        Allowed.Numbers +
-        Allowed.Lowers +
-        Allowed.Symbols
+	length,
+	charset = Allowed.Uppers +
+		Allowed.Numbers +
+		Allowed.Lowers +
+		Allowed.Symbols
 ) {
-    const crypto = window.crypto || window.msCrypto;
+	const crypto = window.crypto || window.msCrypto;
 
-    if (typeof crypto === "undefined") {
-        throw new Error(
-            "Crypto API is not supported. Please upgrade your web browser"
-        );
-    }
+	if (typeof crypto === "undefined") {
+		throw new Error(
+			"Crypto API is not supported. Please upgrade your web browser"
+		);
+	}
 
-    // const charset =
-    //     Allowed.Uppers + Allowed.Numbers + Allowed.Lowers + Allowed.Symbols;
+	// const charset =
+	//     Allowed.Uppers + Allowed.Numbers + Allowed.Lowers + Allowed.Symbols;
 
-    const indexes = crypto.getRandomValues(new Uint32Array(length));
+	const indexes = crypto.getRandomValues(new Uint32Array(length));
 
-    let secret = "";
+	let secret = "";
 
-    for (const index of indexes) {
-        secret += charset[index % charset.length];
-    }
+	for (const index of indexes) {
+		secret += charset[index % charset.length];
+	}
 
-    return secret;
+	return secret;
 }
 
-const password = generatePassword(16);
+function Bar({ length }) {
+	const password = generatePassword(length);
 
-function Bar() {
-    const [length, setLength] = useState(10);
-    // setLength(len)
-    return (
-        <div>
-            <div className="bar display">
-                <img src={image1} className="lock" alt="lock" />
-                {/* <img src={image2} className="dots" alt="dots" srcset="" /> */}
-                {/* <div className="test">..........</div> */}
-                <div className="dots-input">
-                    {new Array(length).fill(0).map((dot, ind) => {
-                        return <div key={ind} className="dot"></div>;
-                    })}
-                </div>
-                <div className="element">
-                    <img src={image2} className="reset" alt="reset" />
-                    <img src={image3} className="copy" alt="copy" />
-                </div>
-            </div>
-            <div className="bar-active bar">
-                <img src={image1} className="lock" alt="lock" />
-                <div className="password ">{password}</div>
-                <div className="element">
-                    <img src={image2} className="reset" alt="reset" />
-                    <img src={image3} className="copy" alt="copy" />
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div className="bar display">
+				<img src={image1} className="lock" alt="lock" />
+				{/* <img src={image2} className="dots" alt="dots" srcset="" /> */}
+				{/* <div className="test">..........</div> */}
+				<div className="dots-input">
+					{new Array(length).fill(0).map((dot, ind) => {
+						return <div key={ind} className="dot"></div>;
+					})}
+				</div>
+				<div className="element">
+					<img src={image2} className="reset" alt="reset" />
+					<img src={image3} className="copy" alt="copy" />
+				</div>
+			</div>
+			<div className="bar-active bar">
+				<img src={image1} className="lock" alt="lock" />
+				<div className="password ">{password}</div>
+				<div className="element">
+					<img src={image2} className="reset" alt="reset" />
+					<img src={image3} className="copy" alt="copy" />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Bar;
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,28 +1,23 @@
 import React, { useState } from "react";
 
-function Slider() {
-    const [value, setValue] = useState(8);
-    // const value = 8;
-    const HandleChange = (event) => {
-        // console.log(event)
-        // console.log(event.target.value);
-        setValue(event.target.value);
-        // const value = event.target.value;
-    };
-    return (
-        <div className="slider">
-            <input
-            className="data"
-                type="range"
-                min={4}
-                max={32}
-                step={1}
-                defaultValue={value}
-                onChange={HandleChange}
-            />
-            <div className="value"> {value}</div>
-        </div>
-    );
+function Slider({ length, setLength }) {
+	const handleChange = (event) => {
+		setLength(event.target.value);
+	};
+	return (
+		<div className="slider">
+			<input
+				className="data"
+				type="range"
+				min={4}
+				max={32}
+				step={1}
+				defaultValue={length}
+				onChange={handleChange}
+			/>
+			<div className="value">{length}</div>
+		</div>
+	);
 }
 
 export default Slider;
